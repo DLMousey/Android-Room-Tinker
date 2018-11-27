202f3326fb2bcf80f58557804a8776cfca8451ee
DLMousey <tom@llamatorials.com>
2018-11-27 00:42:08 +0000

    Initial commit

---
+++ app/src/main/java/com/enderstudy/roomtinker/ViewModel/WordViewModel.java
@@ -0,0 +1,30 @@
+package com.enderstudy.roomtinker.ViewModel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import com.enderstudy.roomtinker.Entity.Word;
+import com.enderstudy.roomtinker.Repository.WordRepository;
+
+import java.util.List;
+
+public class WordViewModel extends AndroidViewModel {
+    private WordRepository mRepository;
+
+    private LiveData<List<Word>> mAllWords;
+
+    public WordViewModel(Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    public LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    public void insert(Word word) {
+        mRepository.insert(word);
+    }
+}
