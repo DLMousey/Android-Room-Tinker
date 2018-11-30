84fee9f16f42507cb5eab187dea7535410dacbdf
DLMousey <tom@llamatorials.com>
2018-11-30 09:53:11 +0000

    Add description property to word entity

--- app/src/main/java/com/enderstudy/roomtinker/Entity/Word.java
+++ app/src/main/java/com/enderstudy/roomtinker/Entity/Word.java
@@ -4,6 +4,7 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 @Entity(tableName = "word_table")
 public class Word {
@@ -11,15 +12,37 @@ public class Word {
     @PrimaryKey(autoGenerate = true)
     private int id;
 
-    @NonNull
-    @ColumnInfo(name = "word")
+    @NonNull @ColumnInfo(name = "word")
     private String mWord;
 
-    public Word(String word) { this.mWord = word; }
+    @Nullable @ColumnInfo(name = "description")
+    private String mDescription;
 
-    public String getWord() { return this.mWord; }
+    public Word(String word) {
+        this.mWord = word;
+    }
 
-    public void setId(int id) { this.id = id; }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-    public int getId() { return this.id; }
+    public int getId() {
+        return this.id;
+    }
+
+    public void setWord(String word) {
+        this.mWord = word;
+    }
+
+    public String getWord() {
+        return this.mWord;
+    }
+
+    public void setDescription(String description) {
+        this.mDescription = description;
+    }
+
+    public String getDescription() {
+        return this.mDescription;
+    }
 }
